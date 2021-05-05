d802aabd454e0fddc7c372b2af94937f197aa45f
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-05-05 12:08:57 -0400

    Inicio projeto ministore-auth-api

---
+++ src/main/java/com/softwarefoundation/ministore/auth/MinistoreAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.ministore.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistoreAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistoreAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarefoundation/ministore/auth/MinistoreAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarefoundation.ministore.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistoreAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
